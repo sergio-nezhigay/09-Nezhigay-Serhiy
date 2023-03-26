f8609086ed2e4f012430a6219194853394394e44
Sergio Nezhigay <abc1971h@gmail.com>
2023-03-26 12:44:09 +0300

    refactored

--- js/main.js
+++ js/main.js
@@ -1,40 +1,30 @@
-let i;
-// Сформировать массив arrА размерностью (длиной), которую запрашиваем у пользователя (prompt()).
 const resultEl = document.querySelector(".result");
-let task = "<h1> HomeWork #9";
+let task = "<h1> HomeWork #9 </h1>";
 
 const arrLength = +prompt("Give me the size of future array:");
+const arrA = Array.from({ length: arrLength }, () =>
+  Math.floor(Math.random() * 1000)
+);
 
-let arrА = [];
-for (let i = 0; i < arrLength; i++) {
-  const rnd = Math.floor(Math.random() * 1000);
-  arrА.push(rnd);
-}
-
-let arrB = arrА.reduce((acc, current) => {
-  if (isSimple(current)) acc.push(current);
-  return acc;
-}, []);
+const arrB = arrA.filter((n) => isSimple(n));
 
 function isSimple(number) {
-  for (i = 2; i < number; i++) {
+  for (let i = 2; i < number; i++) {
     if (number % i === 0) return false;
   }
-  return true;
+  return number > 1;
 }
 
 function findMax(array) {
-  if (!array.length) return "not defined";
-  return Math.max(...array);
+  return array.length ? Math.max(...array) : "not defined";
 }
 
 function findMin(array) {
-  if (!array.length) return "not defined";
-  return Math.min(...array);
+  return array.length ? Math.min(...array) : "not defined";
 }
 
 task += `<h2>
-arrA: ${arrА.join(", ")}<br>
+arrA: ${arrA.join(", ")}<br>
 arrB: ${arrB.join(", ")}<br>
 max is ${findMax(arrB)}, min is ${findMin(arrB)}</h2>`;
 
